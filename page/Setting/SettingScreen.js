4b9f474fa4f53cf8c0234d285b279009f370ba0d
UI-Mario <13277035969@163.com>
2020-03-05 22:48:51 +0800

    [Livio]: try to create a api to mock data, failed, retry tomorrow

--- page/Setting/SettingScreen.js
+++ page/Setting/SettingScreen.js
@@ -2,6 +2,32 @@ import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
 export default class SettingScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isloading: true,
+      videolistData: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  // TODO
+  getData = () => {
+    const url = 'http://api.bilibili.cn/recommend';
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          isloading: false,
+          videolistData: data,
+        });
+      });
+  };
+
   render() {
     return (
       <View style={styles.container}>
